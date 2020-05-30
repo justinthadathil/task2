bb898b2fe61e60ad6c61f0cfb47ec62801372883
Justin Thadathil <justinthadathil93@hmail.com>
2020-05-30 19:35:11 +0530

    draggable changes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,17 @@
-$( function() {
-    $( "#sortable1, #sortable2" ).sortable({
-      connectWith: ".connectedSortable"
-    }).disableSelection();
-
-    $('#sortable1').sortable({
+$(document).ready(function() {
+    
+  $("#sortable").sortable({
       update: function(event, ui) {
-        var proEntry = $(this).sortable('toArray');
-        /* console.log(proEntry); */
-        localStorage.setItem('proEntry', JSON.stringify(proEntry));  
-        var retrievedProduct = localStorage.getItem('proEntry');
-        console.log('retrievedProduct: ', JSON.parse(retrievedProduct));
-      }
-   });
+          var proEntry = $(this).sortable('toArray', { attribute: 'name' });
+          console.log(proEntry);
+          localStorage.setItem('proEntry', JSON.stringify(proEntry));  
+          var retrievedProduct = localStorage.getItem('proEntry');
+          console.log('retrievedProduct: ', JSON.parse(retrievedProduct));
+        }
+  });
+
+  $("#draggable li").draggable({
+      connectToSortable: "#sortable",
+      helper: "clone"
+  });
 });
